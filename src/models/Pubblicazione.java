c00b4b7b0dd82a07f7a7512fc62a635037d74b0c
Katiacrisci <katcrisci@gmail.com>
2024-01-19 16:57:15 +0100

    first commit

---
+++ src/models/Pubblicazione.java
@@ -0,0 +1,9 @@
+package models;
+
+public abstract class Pubblicazione {
+    private long Isbn;
+    private String title;
+    private int annoDiPubblicazione;
+    private int numeroPagine;
+
+}
---
+++ src/models/Rivista.java
@@ -0,0 +1,5 @@
+package models;
+
+public class Rivista extends  Pubblicazione{
+
+}
